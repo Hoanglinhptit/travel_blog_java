a1daca45c9384f6c8b05732c59032fb6b7cdb1ca
Hoanglinhptit <lehoanglinhptit@gmail.com>
2024-01-08 09:29:41 +0700

    init project

---
+++ src/main/java/com/example/travel_blog_java/TravelBlogJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.travel_blog_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBlogJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBlogJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/travel_blog_java/TravelBlogJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travel_blog_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBlogJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
